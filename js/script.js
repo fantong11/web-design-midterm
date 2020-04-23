b0ce182d6a0069a9497a23cdd6c7880b23b11b32
cmalvin121 <cmalvin121@gmail.com>
2020-04-24 02:40:21 +0800

    smooth scrolling + fixed header

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("head-title");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+//-------------------------------------------------------------------------------------------------------//
+
 function animate_bg(ele, from, to) {
     ele.css("background-color", "rgba(187, 93, 0, " + (from += from > to ? -1 : 1) / 10 + ")"); 
     if(from != to)  
