227e3b437247fdd87882c49e346f0d0e954af1ea
fantong <fankaihsiang11@gmail.com>
2020-04-24 20:31:20 +0800

    Add hover to menu button, and finish the typesetting of the web

--- js/script.js
+++ js/script.js
@@ -18,21 +18,31 @@
 //-------------------------------------------------------------------------------------------------------//
 
 function animate_bg(ele, from, to) {
-    ele.css("background-color", "rgba(187, 93, 0, " + (from += from > to ? -1 : 1) / 10 + ")"); 
-    if(from != to)  
-        setTimeout(function() { animate_bg(ele, from, to) }, 20);
+    ele.css("background-color", "rgba(187, 93, 0, " + (from += from > to ? -1 : 1) / 10 + ")");
+    if (from != to)
+        setTimeout(function () { animate_bg(ele, from, to) }, 20);
 }
 
 // $("header").hover(
 //     function() {return animate_bg($(header), 0, 10)},
 //     function() {return animate_bg($(header), 10, 0)}
 // );
-$(document).scroll(function() { 
-    if($(window).scrollTop() != 0) {
-        //animate_bg($('header'), 0, 10)
-        animate_bg($('header'), 10, 0)
-    }
-    // else {
-    //     animate_bg($('header'), 10, 0)
-    // }
- });
+// $(document).scroll(function () {
+//     if ($(window).scrollTop() != 0) {
+//         animate_bg($('header'), 0, 10)
+//         animate_bg($('header'), 10, 0)
+//     }
+//     else {
+//         animate_bg($('header'), 10, 0)
+//     }
+// });
+
+/*當滑鼠放上選單時會變顏色*/
+$(".nav-list-item-link").hover(function () {
+    // 滑鼠在上面的時候
+    $(this).css("background", "#d19b05");
+},
+function() {
+    // 滑鼠不在上面的時候
+    $(this).css("background", "#e7ab06");
+});
